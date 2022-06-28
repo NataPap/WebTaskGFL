6ddb4dea50bf7797f593c4111289c693a7dd02bd
NataPap <papushynanataliya@gmail.com>
2022-06-28 10:49:28 +0300

    Added request Delete and Edit

--- src/main/java/com/example/webtaskgfl/controllers/UserController.java
+++ src/main/java/com/example/webtaskgfl/controllers/UserController.java
@@ -7,9 +7,7 @@ import com.example.webtaskgfl.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class UserController {
@@ -27,8 +25,9 @@ public class UserController {
         return "resultList";
     }
     @PostMapping("/add")
-    public String addExpression(@RequestParam User user) {
+    public String addUser (@ModelAttribute User user, Model model) {
         try {
+            model.addAttribute("user", user);
             User userNew = new User();
             userNew.setId(user.getId());
             userNew.setName(user.getName());
@@ -43,8 +42,31 @@ public class UserController {
             userNew.setAddress(addressNew);
             userNew.setPhone(user.getPhone());
             userRepository.save(userNew);
-
         } catch (Exception ignored) {}
             return "redirect:/list";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteUser(@PathVariable("id") int id) {
+        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("invalid user id"));
+        userRepository.delete(user);
+
+        return "redirect:/list";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showUpdatePage(@PathVariable("id") int id, Model model) {
+        User user = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("invalid user id"));
+        model.addAttribute("user", user);
+        return "edit";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateUser(@PathVariable("id") int id, User user) {
+        try {
+            userRepository.save(user);
+        } catch (Exception ignored){}
+
+        return "redirect:/list";
     }
 }
